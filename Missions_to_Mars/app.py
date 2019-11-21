814ddb49f27f47b028e2c288b854cac1693af7d3
Cheikh Sidi <14164306+cheikhsidi@users.noreply.github.com>
2019-11-20 19:51:38 -0500

    adding my app

--- Missions_to_Mars/app.py
+++ Missions_to_Mars/app.py
@@ -1,49 +1,37 @@
-from flask import Flask, jsonify, render_template
-import pymongo
+from flask import Flask, redirect, render_template
+from flask_pymongo import PyMongo
 import scrape_mars
 
 app = Flask(__name__)
 
 
-# Create connection variable
-conn = 'mongodb://localhost:27017'
-
-# Pass connection to the pymongo instance.
-client = pymongo.MongoClient(conn)
-
-# Connect to a database. Will create one if not already available.
-db = client.Mars_db
-
-# Drops collection if available to remove duplicates
-db.Mars_db.drop()
-
-# Creates a collection in the database and inserts two documents
-#db.Mars.insert_one()
+# Use flask_pymongo to set up mongo connection
+app.config["MONGO_URI"] = "mongodb://localhost:27017/Mars_db"
+mongo = PyMongo(app)
 
 # create route that renders index.html template
+@app.route("/")
+def index():
+    # Find one record of data from the mongo database
+    Mars_data = mongo.db.Mars_data.find_one()
 
+    # Return template and data
+    return render_template("index.html", Mars_data=Mars_data)
 
-@app.route("/scrape/")
 # Route that will trigger the scrape function
 @app.route("/scrape")
 def scrape():
 
     # Run the scrape function
-    mars = scrape_mars.scrape()
+    Mars = mongo.db.Mars_data
+    Mars_data = scrape_mars.scrape()
 
     # Update the Mongo database using update and upsert=True
-    Mars_db.db.collection.update({}, mars, upsert=True)
+    mongo.db.collection.update({}, Mars_data, upsert=True)
 
     # Redirect back to home page
     return redirect("/")
 
-@app.route("/")
-def index():
-    # Find one record of data from the mongo database
-    Mars_data = Mars_db.db.collection.find_one()
-
-    # Return template and data
-    return render_template("index.html", vacation=Mars_data)
 
 if __name__ == "__main__":
     app.run(debug=True)
